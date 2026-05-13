f529a40acd5c275abdc436e039acd877b37ac9bb
Magali <magaliaylen05@gmail.com>
2026-05-13 02:35:14 -0300

    Login terminado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgramacionV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
